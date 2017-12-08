c41ad3f5bd85645f8f67a8da2314f451c4831423
sibaihm <sibaihm@gmail.com>
2017-12-08 08:13:30 +0100

    first commit

---
+++ src/com/sibaihm/yandextranslate/yandextranslate.java
@@ -0,0 +1,153 @@
+
+/*
+ *
+ *
+ *	Created by Sibai Haj Mousa 2017
+ *	Version 1.0
+ *
+ *
+ *	Licensed under the GNU General Public License v3.0
+ *	You can obtain a copy of the license under:
+ *	https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ *
+ */
+
+
+package com.sibaihm.yandextranslate;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
+
+public class YandexTranslate extends AsyncTask<String, Void, String> {
+
+	/*
+     *	Your Yandex API Key
+     */
+	
+    private final String API_KEY     = "";
+	
+	/*
+     *	Yandex API Service URL
+     */
+	
+    private final String YANDEX_URL  = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=";
+
+
+    @Override
+    protected String doInBackground(String... params){
+
+        /*
+    	 * 	The text which will be translated
+    	 */
+
+        final String textToTranslate = params[0];
+        
+        /*
+         *	The source language to be translated from
+         */
+        
+        final String SOURCE_LANGUAGE = params[1];
+        
+        /*
+         *	The wished language to be translated to
+         */
+        
+        final String TARGET_LANGUAGE = params[2];
+		
+        try {
+			
+			/*
+             *	Encoding the word in UTF-8
+			 *	Use it if you get encoding problems
+             */
+			
+			wordToTranslate = URLEncoder.encode(wordToTranslate, "UTF-8");
+			
+			/*
+			 *	Creating the URL object with the url value
+			 */
+
+            URL url = new URL(YANDEX_URL + API_KEY + "&text=" + textToTranslate + "&lang=" + SOURCE_LANGUAGE + "-" + TARGET_LANGUAGE);
+
+			/*
+			 *	Creating Http Connection, Input Stream, Buffered Reader and jsonString
+			 */
+			
+            HttpURLConnection connection    = (HttpURLConnection) url.openConnection();
+			
+            InputStream inputStream         = connection.getInputStream();
+			
+            BufferedReader bufferedReader   = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+			
+            String jsonString;
+
+			/*
+			 *	Creating string builder and inserting retrieved JSON result into it
+			 */
+			
+            StringBuilder stringBuilder = new StringBuilder();
+			
+            while ((jsonString = bufferedReader.readLine()) != null) {
+				
+                stringBuilder.append(jsonString + "\n");
+				
+            }
+
+			/*
+			 *	Closing and disconnecting
+			 */
+			
+            bufferedReader.close();
+			
+            inputStream.close();
+			
+            connection.disconnect();
+
+            /*
+             * 	Triming the response and removing any special chars from it
+             *	The response has the form of: {"code":200,"lang":"de-ar","text":["blabla"]}
+             */
+			
+            JSONObject obj = new JSONObject(stringBuilder.toString().trim());
+			
+            if(obj.getString("code").equals("200")) {
+				
+                String resultString = obj.getString("text");
+				
+				/*
+			 	 *	Escaping  ,"[]  from the result
+			 	 */
+				
+                resultString = resultString.replaceAll("[,\"\\[\\]]", "");
+				
+                return resultString;
+				
+            }
+
+            /*
+             * 	Returning empty string if there is any error (response code is other than 200)
+             */
+			
+            else return "";
+
+        } catch (Exception e){
+			
+            Log.e("Yandex Response ", e.getMessage());
+			
+            return "";
+			
+        }
+
+    }
+
+}
